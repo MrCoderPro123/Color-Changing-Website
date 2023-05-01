b2e3447c7960e87a6950c42ad11efb91fae37217
MrCoderPro123 <ahmedegamer23@gmail.com>
2023-05-01 21:54:38 +0500

    Color Changer

---
+++ app.js
@@ -0,0 +1,14 @@
+let changeColor = (color)=>{
+    document.body.style.backgroundColor = color;
+}
+
+let changeImg = ()=>{
+   let ci = document.getElementById('1img');
+//    ci.setAttribute('src','./whatsapp.png');
+    if(ci.src == "./whatsapp.png"){
+        ci.src = "./youtube.png";
+    }else{
+        ci.setAttribute('src','./whatsapp.png');
+    }
+
+}
