b1a4aaf4c928f610fafa5e81733ec794bfea6657
MrCoderPro123 <ahmedegamer23@gmail.com>
2023-05-05 20:13:12 +0500

    added progress bar

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-let changeColor = (color)=>{
+const loadBar = document.getElementById("loadBar");
+
+
+
+let i = 0;
+let intervelId = setInterval(() => {
+    i++
+    loadBar.style.width = i + "vw";
+    if (i === 100) {
+        clearInterval(intervelId)
+    }
+}, 50);
+
+
+
+
+let changeColor = (color) => {
     document.body.style.backgroundColor = color;
 }
 
-let changeImg = ()=>{
-   let ci = document.getElementById('1img');
-//    ci.setAttribute('src','./whatsapp.png');
-    if(ci.getAttribute("src") == "./whatsapp.png"){
+let changeImg = () => {
+    let ci = document.getElementById('1img');
+    //    ci.setAttribute('src','./whatsapp.png');
+    if (ci.getAttribute("src") == "./whatsapp.png") {
         ci.setAttribute("src", "./youtube.png");
-    }else{
-        ci.setAttribute('src','./whatsapp.png');
+    } else {
+        ci.setAttribute('src', './whatsapp.png');
     }
 
 }
